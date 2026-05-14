3afb0f31cff93b9079e02d8c07998a11f894a2b5
Abiral <abiralbhattarai101@gmail.com>
2026-05-14 14:28:11 +0545

    fix: changes to llm port

--- src/ports/output/llm_port.py
+++ src/ports/output/llm_port.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
-from src.domain.models.analysis import VideoExtractionData, FrameAnalysis
+from ...domain.models.analysis import VideoExtractionData, FrameAnalysis
 
 
 class LlmPort(ABC):
     """Interface for the LLM port to generate analysis results based on extracted video data."""
 
     @abstractmethod
-    def generate(self, video_data: VideoExtractionData) -> FrameAnalysis:
+    def generate_batch_frame_analysis(self, video_data: VideoExtractionData) -> FrameAnalysis:
         """Generates analysis results based on the provided video extraction data."""
         pass
